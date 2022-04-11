65437179b907b77f6d914aeae9869455804278b2
DYNAMICPOSH <realpatrick11@gmail.com>
2022-04-11 01:32:40 +0000

    my commit message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 int n;
 
 srand(time(0));
-n = rand() - RAND_MAX / 2:
+n = rand() - RAND_MAX / 2;
 
 if (n % 10 > 5)
 {
@@ -28,7 +28,6 @@ else
 {
 print("Last digit of %i is %i and is less than 6 and not o\n", n, n % 10);
 }
-
 
 return(0);
 }
