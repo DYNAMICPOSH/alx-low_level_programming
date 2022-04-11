b18ed469bdef5eb4b712df559bdb09874fa821fe
DYNAMICPOSH <realpatrick11@gmail.com>
2022-04-11 02:08:51 +0000

    my commit message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 
 else if (n % 10 == 0)
 {
-printf("Last digit of %i is %i an is o\n", n, n % 10);
+printf("Last digit of %i is %i and is o\n", n, n % 10);
 }
 
 else
